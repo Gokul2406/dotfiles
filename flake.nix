36f5dea92f612bb05be9afcfa0697065412bec54
Gokul2406 <gokulpbharathan89@gmail.com>
2023-12-16 17:26:58 +0530

    Initial commit after rust

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "first flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, ... }
+  : let
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+     nixos = lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
